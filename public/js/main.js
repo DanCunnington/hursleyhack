5b0952daef16412f11e462a25cb94811c3a93fae
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-01-24 21:35:45 +0000

    buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
 	$("button.signup").click(function() {
-		window.open('https://w3-connections.ibm.com/communities/service/html/communityview?communityUuid=f737c663-974c-4b8d-92ab-51df9c97767b#fullpageWidgetId=W408e59db464d_42d5_9720_790c1e0e5d69&eventInstUuid=6a9a718e-2c40-4c4d-88a3-b604d38c98d4');
+		window.open('https://docs.google.com/forms/d/1LEo1B0uLuzGq5C3JfFNxtwxZGLIGRFaYbiHUagVCCl8/viewform');
+	});
+
+	$("button.team-signup").click(function() {
+		window.open('https://docs.google.com/forms/d/1Qlz7KJFmb1WuH-y_Iq5sus0-wJOqPlE_kUyPvhMyVBQ/viewform');
 	});
 
 	$("ul.nav li").click(function() {
