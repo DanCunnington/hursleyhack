c27fe3c2a0201f3bc4b0579a8e53ed53f389e3e8
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-01-28 10:10:29 +0000

    timer

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,49 @@ $(document).ready(function() {
 	    }, 1000);
 	});
 
+	var deadline = 'February 7 2016 23:59:59 GMT+00:00';
+	initializeClock(deadline);
+
+
+
 });
 
 
+function initializeClock(endtime){
+  //var clock = document.getElementById(id);
+  var timeinterval = setInterval(function(){
+    var t = getTimeRemaining(endtime);
+    // clock.innerHTML = t.days + ' Days  ' +
+    //                   t.hours + ' Hours  ' +
+    //                   t.minutes + ' Minutes  ' +
+    //                   t.seconds + ' Seconds  ';
 
+
+    $("#days").html(t.days);                 
+    $("#hours").html(t.hours);                 
+    $("#minutes").html(t.minutes);                 
+    $("#seconds").html(t.seconds);
+    $("h4.clock").show();                
+
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
 
